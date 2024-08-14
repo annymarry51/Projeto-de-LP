6a967a5d3042b9328e0f8e3acf03bf60a213db01
Ana Maria <anamariadecarvalhomendonca51@gmail.com>
2024-08-14 13:59:06 -0300

    Atualizando...

--- main.hs
+++ main.hs
@@ -1,39 +1,10 @@
 module Main where
 
 import System.IO
-import Text.XHtml (content)
-
 
 type NomeAutor = String
 type Titulo = String
 type Livro = (Titulo, NomeAutor, Bool)
-
---------------------------------------------------
---função para separar cada livro
-leLivro :: String -> String
-leLivro [] = []
-leLivro (x:xs)
-  |x /= '\n' = x: leLivro xs
----------------------------------------------------
-separaInformaAux :: String -> String
-separaInformaAux [] = error "Lista Vazia"
-separaInformaAux (x:xs) 
-  | x== ';' || x == ',' = x
-  | otherwise = x:separaInformaAux xs
-
---função para separar as informações de cada livro
-separaInforma :: String -> Livro
-separaInforma [] = error "Lista Vazia"
-separaInforma  (x:xs) 
-  | x== ',' = (,_,)
-  | x == ';' = (,x,)
-  | x == '\n'
-  | otherwise = separaInforma xs
-  where 
----------------------------------------------------
-meusLivros :: String -> [Livro]
-meusLivros conteudo = separaInforma leLivro conteudo
----------------------------------------------------
 
 ----------------------------------------------------
 --função para buscar livro
@@ -47,11 +18,4 @@ buscaLivro ((titulo, autor, emprestado): resto) nome
 
 main :: IO()
 main = do
-    conteudo <- readFile "meuarquivo.txt"
-    putStrLn "-----SEJA BEM VINDO AO SIBLIB-----\n"
-    putStrLn "|  O QUE DESEJA FAZER?           |\n"
-    putStrLn "|  1 - BUSCA DE LIVRO            |\n"
-    putStrLn "|  2 - EMPRESTIMO DE LIVRO       |\n"
-    putStrLn "|  3 - DEVOLUCAO DE LIVRO        |\n"
-    putStrLn "|  0 - SAIR                      |\n"
-    putStrLn "----------------------------------"
+   print ()
