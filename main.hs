6bf0073c78a1e1c5fd4b4492237e1357ebc85b7d
Ana Maria <anamariadecarvalhomendonca51@gmail.com>
2024-08-14 14:37:06 -0300

    Nova definição do tipo livro

--- main.hs
+++ main.hs
@@ -4,15 +4,29 @@ import System.IO
 
 type NomeAutor = String
 type Titulo = String
-type Livro = (Titulo, NomeAutor, Bool)
 
-----------------------------------------------------
+
+-- Definição do tipo de dados Livro
+data Livro = Livro {
+    titulo :: Titulo,
+    autor :: NomeAutor,
+    emprestado :: Bool
+} deriving Show
+----------------------------------------------------------------
+--livros que tem na biblioteca
+livrosDaBiblioteca :: [Livro]
+livrosDaBiblioteca = [
+    Livro "O sertão vai virar mar" "Moacyr Scliar" True,
+    Livro "Água viva" "Clarice Lispector" True,
+    Livro "Terror na Festa" "Janaína Amado" True
+    ]
+----------------------------------------------------------------
 --função para buscar livro
 buscaLivro ::  [Livro] -> String -> [Livro]
 buscaLivro [] _ = []
-buscaLivro ((titulo, autor, emprestado): resto) nome
-  | titulo == nome = (titulo, autor, emprestado) : buscaLivro resto nome
-  | autor == nome = (titulo, autor, emprestado) : buscaLivro resto nome
+buscaLivro (livro: resto) nome
+  | titulo livro == nome = livro : buscaLivro resto nome
+  | autor livro == nome = livro : buscaLivro resto nome
   | otherwise = buscaLivro resto nome
 ---------------------------------------------------
 
