1a89d21880ae3aad16b4542d15d46867b24365ae
Ana Maria <anamariadecarvalhomendonca51@gmail.com>
2024-08-14 17:57:12 -0300

    menu

--- main.hs
+++ main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.IO
+import System.Environment (getArgs)
 
 type NomeAutor = String
 type Titulo = String
@@ -12,6 +13,7 @@ data Livro = Livro {
     autor :: NomeAutor,
     emprestado :: Bool
 } deriving Show
+
 ----------------------------------------------------------------
 --livros que tem na biblioteca
 livrosDaBiblioteca :: [Livro]
@@ -20,6 +22,8 @@ livrosDaBiblioteca = [
     Livro "Água viva" "Clarice Lispector" True,
     Livro "Terror na Festa" "Janaína Amado" True
     ]
+----------------------------------------------------------------
+--emprestimoLivro :: [Livro]
 ----------------------------------------------------------------
 --função para buscar livros
 buscaLivros ::  [Livro] -> String -> [Livro]
@@ -36,7 +40,45 @@ buscaLivro (livro: resto) nome escritor =
   if titulo livro == nome &&  autor livro == escritor then livro
   else buscaLivro resto nome escritor
 ----------------------------------------------------------------
+--função para emprestar livro
+--emprestimoLivro :: (buscaLivro) -> [Livro]
+--emprestimoLivro buscaLivro = buscaLivro !emprestado 
+-----------------------------------------------------------------
+-- opções do menu
+--opcao1 :: IO ()
 
+-----------------------------------------------------------------
+--menu do sistema...
+
+menu :: IO ()
+menu = do
+    putStrLn "-----SEJA BEM VINDO AO SIBLIB-----\n"
+    putStrLn "|  O QUE DESEJA FAZER?           |\n"
+    putStrLn "|  1 - BUSCA DE LIVRO            |\n"
+    putStrLn "|  2 - EMPRESTIMO DE LIVRO       |\n"
+    putStrLn "|  3 - DEVOLUCAO DE LIVRO        |\n"
+    putStrLn "|  0 - SAIR                      |\n"
+    putStrLn "----------------------------------"
+    hFlush stdout
+    args <- readLn :: IO Int
+    case args of
+      1 -> do
+        putStrLn "Você escolheu a opção 1"
+        menu
+      2 -> do
+        putStrLn "Você escolheu a opção 2"
+        menu
+      3 -> do
+        putStrLn "Você escolheu a opção 3"
+        menu
+      0 -> do
+        putStrLn "Saindo..."
+      _ -> do
+        putStrLn "Opção inválida...."
+        menu
+
+    
+-------------------------------------------------------------------
 main :: IO()
 main = do
-   print ()
+  menu
