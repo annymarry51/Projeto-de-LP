88a45807c90d41d67e007c58231734a7b494a949
Ana Maria <anamariadecarvalhomendonca51@gmail.com>
2024-08-14 14:48:15 -0300

    função para buscar livro

--- main.hs
+++ main.hs
@@ -21,14 +21,21 @@ livrosDaBiblioteca = [
     Livro "Terror na Festa" "Janaína Amado" True
     ]
 ----------------------------------------------------------------
---função para buscar livro
-buscaLivro ::  [Livro] -> String -> [Livro]
-buscaLivro [] _ = []
-buscaLivro (livro: resto) nome
-  | titulo livro == nome = livro : buscaLivro resto nome
-  | autor livro == nome = livro : buscaLivro resto nome
-  | otherwise = buscaLivro resto nome
----------------------------------------------------
+--função para buscar livros
+buscaLivros ::  [Livro] -> String -> [Livro]
+buscaLivros [] _ = []
+buscaLivros (livro: resto) nome
+  | titulo livro == nome = livro : buscaLivros resto nome
+  | autor livro == nome = livro : buscaLivros resto nome
+  | otherwise = buscaLivros resto nome
+----------------------------------------------------------------
+--função para buscar um livro específico
+buscaLivro ::  [Livro] -> String -> String-> Livro
+buscaLivro [] _ _ = Livro " " " " False
+buscaLivro (livro: resto) nome escritor =
+  if titulo livro == nome &&  autor livro == escritor then livro
+  else buscaLivro resto nome escritor
+----------------------------------------------------------------
 
 main :: IO()
 main = do
